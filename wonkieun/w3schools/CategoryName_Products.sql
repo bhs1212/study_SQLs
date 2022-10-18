9e12c5750781bfaccf9073b9ae4a730a21f144c8
keamy-eun <eun.wonki014@gmail.com>
2022-10-18 13:29:32 +0900

    wonki

--- wonkieun/w3schools/CategoryName_Products.sql
+++ wonkieun/w3schools/CategoryName_Products.sql
@@ -1,5 +1,6 @@
-SELECT SUM(Price BETWEEN 10.00 AND 50.00), AVG(Price BETWEEN 10.00 AND 50.00)
+SELECT SUM(Price), AVG(Price)
 FROM Products
 WHERE CategoryID in (SELECT CategoryID
 					FROM Categories
-					WHERE CategoryName in ('Dairy Products' ,'Seafood'))
+					WHERE CategoryName in ('Dairy Products' ,'Seafood')
+                    AND Price BETWEEN 10.00 AND 50.00)
