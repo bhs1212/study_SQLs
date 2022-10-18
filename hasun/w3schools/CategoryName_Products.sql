7281087e4156c522a31bc116ac47be0d0f366eec
bhs1212 <hasun17kh@gmail.com>
2022-10-18 12:45:03 +0900

    hasun

--- hasun/w3schools/CategoryName_Products.sql
+++ hasun/w3schools/CategoryName_Products.sql
@@ -1,17 +1,8 @@
---CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 합계 ?
-SELECT SUM(Price)
+--CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 합계와 평균은 ?
+SELECT SUM(Price), AVG(Price)
 FROM Products
 WHERE CategoryID in (SELECT CategoryID
 					FROM Categories
                     WHERE CategoryName in ('Dairy Products', 'Seafood'))
 AND Price BETWEEN 10 AND 50;
---390.34
-
---CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 평균은 ?
-SELECT AVG(Price)
-FROM Products
-WHERE CategoryID in (SELECT CategoryID
-					FROM Categories
-                    WHERE CategoryName in ('Dairy Products', 'Seafood'))
-AND Price BETWEEN 10 AND 50;
--- 24.396250
+-- 합계 : 390.34, 평균 : 24.396250
